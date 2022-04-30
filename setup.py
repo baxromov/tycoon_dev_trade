54f9a77e11572b686981d620a6e4e0ca9d26ad2a
baxromov <baxromov.shahzodbek@gmail.com>
2022-04-30 19:16:55 +0500

    add

--- setup.py
+++ setup.py
@@ -2,14 +2,9 @@ import pathlib
 
 from setuptools import setup, find_packages
 
-# The directory containing this file
 HERE = pathlib.Path(__file__).parent
 
-# The text of the README file
 README = (HERE / "README.md").read_text()
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-# This call to setup() does all the work
 
 setup(
     name="tycoon-dev-trade",
